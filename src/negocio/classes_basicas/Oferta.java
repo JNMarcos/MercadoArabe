6e9bc59134f8c42b5ed0cd8685fc7f2855eaceed
JNMarcos <marcos_joaos@hotmail.com>
2015-05-08 11:20:05 -0300

    Adição dos atributos e métodos set/get em Oferta

--- src/negocio/classes_basicas/Oferta.java
+++ src/negocio/classes_basicas/Oferta.java
@@ -1,5 +1,22 @@
 package negocio.classes_basicas;
 
+import java.sql.Time;
+
 public class Oferta extends Produto{
+	private int porcentagem;
+	private Time tempo;
+	
+	public int getPorcentagem() {
+		return porcentagem;
+	}
+	public void setPorcentagem(int porcentagem) {
+		this.porcentagem = porcentagem;
+	}
+	public Time getTempo() {
+		return tempo;
+	}
+	public void setTempo(Time tempo) {
+		this.tempo = tempo;
+	}
 
 }
