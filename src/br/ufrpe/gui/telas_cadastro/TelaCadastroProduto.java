213b2e8a18a6b541e08d1713373e7501e166247b
João Lucas Santos <joao__lucas_santos@hotmail.com>
2015-06-10 21:02:50 -0300

    Teste

--- src/br/ufrpe/gui/telas_cadastro/TelaCadastroProduto.java
+++ src/br/ufrpe/gui/telas_cadastro/TelaCadastroProduto.java
@@ -1,7 +1,75 @@
 package br.ufrpe.gui.telas_cadastro;
 
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
+import java.awt.SystemColor;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
-public class TelaCadastroProduto extends JFrame{
+public class TelaCadastroProduto {
 
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaCadastroProduto window = new TelaCadastroProduto();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TelaCadastroProduto() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(SystemColor.activeCaption);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Pr\u00F3ximo");
+		btnNewButton.setBounds(170, 227, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JLabel lblNome = new JLabel("Nome:");
+		lblNome.setBounds(24, 28, 46, 14);
+		frame.getContentPane().add(lblNome);
+		
+	
+		
+		JLabel lblDescrio = new JLabel("Descri\u00E7\u00E3o:");
+		lblDescrio.setBounds(10, 76, 77, 14);
+		frame.getContentPane().add(lblDescrio);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(77, 73, 295, 68);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(80, 25, 292, 20);
+		frame.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+	}
 }
---
+++ src/br/ufrpe/gui/telas_cadastro/TelaCadastroProduto2.java
@@ -0,0 +1,58 @@
+package br.ufrpe.gui.telas_cadastro;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.SystemColor;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class TelaCadastroProduto2 {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaCadastroProduto2 window = new TelaCadastroProduto2();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TelaCadastroProduto2() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(SystemColor.activeCaption);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblPreo = new JLabel("Pre\u00E7o:");
+		lblPreo.setBounds(10, 46, 46, 14);
+		frame.getContentPane().add(lblPreo);
+		
+		textField = new JTextField();
+		textField.setBounds(66, 43, 69, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+	}
+
+}
