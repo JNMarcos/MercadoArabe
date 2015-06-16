2b338643e7c3784432edc2da6fa135d80e066e80
carlosolimpio <carlossolimpio@hotmail.com>
2015-06-16 18:24:30 -0300

    Criado método para venda de produto

--- src/br/ufrpe/negocio/cadastros/CadastroProduto.java
+++ src/br/ufrpe/negocio/cadastros/CadastroProduto.java
@@ -5,6 +5,8 @@ import java.util.List;
 import br.ufrpe.dados.IRepositorioProduto;
 import br.ufrpe.dados.RepositorioProduto;
 import br.ufrpe.negocio.classes_basicas.Produto;
+import br.ufrpe.negocio.classes_basicas.Vendedor;
+import br.ufrpe.negocio.classes_basicas.Xp;
 import br.ufrpe.negocio.exceptions_negocio.NaoEncontradoProdutoException;
 import br.ufrpe.negocio.exceptions_negocio.ProdutoJaCadastradoException;
 
@@ -36,4 +38,14 @@ public class CadastroProduto {
 		return repositorio.exibirInfoProduto(nomeProduto);
 	}
 
+	public void vendeuProduto(Produto produto, Vendedor vendedor) throws NaoEncontradoProdutoException {
+
+		repositorio.procurarProdutoPorNome(produto.getNome());
+
+		Xp xp = vendedor.getXp();
+		xp.adicionarPontosPorVender(produto); //atualiza pts e muda estado do produto p/ vendido(true)
+		vendedor.setXp(xp); 				  //atualizando o xp do vendedor
+
+		repositorio.removerProduto(produto.getNome()); //remove
+	}
 }
