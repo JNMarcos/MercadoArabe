2b967160a5a23a053f94ca661202577e1fde608d
carlosolimpio <carlossolimpio@hotmail.com>
2015-06-29 02:25:29 -0300

    Adicionado eventos

--- src/br/ufrpe/gui/telas_principais/TelaLoginComprador.java
+++ src/br/ufrpe/gui/telas_principais/TelaLoginComprador.java
@@ -1,37 +1,33 @@
 package br.ufrpe.gui.telas_principais;
 
-import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-import java.awt.SystemColor;
+import br.ufrpe.negocio.Fachada;
+import br.ufrpe.negocio.classes_basicas.Comprador;
+import br.ufrpe.negocio.exceptions_negocio.NaoEncontradoCompradorException;
+import br.ufrpe.negocio.exceptions_negocio.NaoEncontradoVendedorException;
+import br.ufrpe.negocio.exceptions_negocio.SenhaIncorretaException;
 
 public class TelaLoginComprador {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TelaLoginComprador window = new TelaLoginComprador();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	private JTextField textField_User;
+	private JPasswordField passwordField;
+	private TelaComprador telaComprador;
+	private Fachada fachada;
+	private Comprador comprador;
+	
 	/**
 	 * Create the application.
 	 */
@@ -46,46 +42,94 @@ public class TelaLoginComprador {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(SystemColor.activeCaption);
 		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-	
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 434, 261);
+		panel.setLayout(null);
+		frame.getContentPane().add(panel);
+		
 		JLabel lblVendedor = new JLabel("Comprador");
 		lblVendedor.setFont(new Font("Franklin Gothic Demi Cond", Font.PLAIN, 18));
 		lblVendedor.setBounds(173, 31, 89, 31);
-		frame.getContentPane().add(lblVendedor);
-		
-		JButton btnEntrar = new JButton(" Entrar");
-		btnEntrar.setBounds(250, 211, 89, 23);
-		btnEntrar.setFont(new Font("Gisha", Font.PLAIN, 13));
-		frame.getContentPane().add(btnEntrar);
-		
-		JButton btnVoltar = new JButton("Voltar ");
-		btnVoltar.setBounds(87, 211, 89, 23);
-		btnVoltar.setFont(new Font("Gisha", Font.PLAIN, 13));
-		frame.getContentPane().add(btnVoltar);
+		panel.add(lblVendedor);
 		
 		JLabel lblUsurio = new JLabel("Usu\u00E1rio:");
-		lblUsurio.setBounds(87, 85, 46, 14);
+		lblUsurio.setBounds(87, 84, 50, 14);
 		lblUsurio.setFont(new Font("Gisha", Font.PLAIN, 13));
-		frame.getContentPane().add(lblUsurio);
+		panel.add(lblUsurio);
+		
+		textField_User = new JTextField();
+		textField_User.setBounds(143, 81, 200, 23);
+		panel.add(textField_User);
+		textField_User.setColumns(10);
 		
 		JLabel lblSenha = new JLabel("Senha:");
 		lblSenha.setBounds(87, 120, 46, 14);
 		lblSenha.setFont(new Font("Gisha", Font.PLAIN, 13));
-		frame.getContentPane().add(lblSenha);
+		panel.add(lblSenha);
 		
-		textField = new JTextField();
-		textField.setBounds(143, 81, 200, 23);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		passwordField = new JPasswordField();
+		passwordField.setBounds(143, 117, 200, 23);
+		panel.add(passwordField);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(143, 116, 200, 23);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.setBounds(254, 164, 89, 23);
+		btnEntrar.setFont(new Font("Gisha", Font.PLAIN, 13));
+		panel.add(btnEntrar);
 		
+		//liga btn entrar
+		fachada = new Fachada();
+		comprador = new Comprador();
+		EventoBotaoEntrar acaoBtnEntrar = new EventoBotaoEntrar();
+		btnEntrar.addActionListener(acaoBtnEntrar);
 		
+		JButton btnVoltar = new JButton("Voltar ");
+		btnVoltar.setBounds(143, 164, 89, 23);
+		btnVoltar.setFont(new Font("Gisha", Font.PLAIN, 13));
+		panel.add(btnVoltar);
+		
+		//liga botao voltar
+		EventoBotaoVoltar acaoBtnVoltar = new EventoBotaoVoltar();
+		btnVoltar.addActionListener(acaoBtnVoltar);
 	}
 	
-
+	public void setVisible(boolean opcao) {
+		if(opcao == true)
+			frame.setVisible(opcao);
+		else
+			frame.setVisible(opcao);
+	}
+	
+	private class EventoBotaoEntrar implements ActionListener {
+		public void actionPerformed(ActionEvent evento) {
+			try {
+				String senha = new String(passwordField.getPassword());
+				
+				comprador = fachada.verificarLoginComprador(textField_User.getText(), senha); //obs aqui
+				telaComprador.setVisible(true);
+				
+				//mensagem boas vindas
+				JOptionPane.showMessageDialog(null, "Bem vindo, " + textField_User.getText() + "!");
+				textField_User.setText("");
+				passwordField.setText("");
+				frame.dispose(); //volta p/ tela inicio
+				
+			} catch(NaoEncontradoCompradorException e) {
+				JOptionPane.showMessageDialog(null, "Comprador não encontrado! Tente novamente.");
+				textField_User.setText("");
+				passwordField.setText("");
+			} catch(SenhaIncorretaException e) {
+				JOptionPane.showMessageDialog(null, "Senha incorreta! Tente novamente.");
+				passwordField.setText("");
+			}
+		}
+	}
+	
+	private class EventoBotaoVoltar implements ActionListener {
+		public void actionPerformed(ActionEvent evento) {
+			frame.setVisible(false);
+		}
+	}
 }
